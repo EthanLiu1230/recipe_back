5af516e592b66fc7807b4ef96f6ee341a1aac6fe
Ethan Liu <liuyuxuan1230@outlook.com>
2020-01-28 01:31:09 +0800

    Email must have validation.

--- core/models.py
+++ core/models.py
@@ -7,6 +7,9 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create a new user profile object."""
+        if not email:
+            raise ValueError('User must have an email.')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
