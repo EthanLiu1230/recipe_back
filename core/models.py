43b8d50427c9ba4a7fb1678563752ba486496685
Ethan Liu <liuyuxuan1230@outlook.com>
2020-01-28 01:26:11 +0800

    Add Custom user & migrate.

--- core/models.py
+++ core/models.py
@@ -5,35 +5,16 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 class UserManager(BaseUserManager):
     """Helps Django work with our custom user model."""
 
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         """Create a new user profile object."""
-
-        if not email:
-            raise ValueError('Users must have an email address.')
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
-
-        # store hash of password in db instead
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, name, password):
-        """Creates and saves a new superuser."""
 
-        user = self.create_user(email, name, password)
-
-        user.is_superuser = True
-        user.is_staff = True
-
-        user.save(using=self._db)
-
-        return user
-
-
-class UserProfile(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """create to substitute default django user"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
@@ -44,17 +25,8 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
-    def get_full_name(self):
-        """Used to get a user's full name"""
-        return self.name
 
-    def get_short_name(self):
-        return self.name
-
-    def __str__(self):
-        return self.email
-
-
-"""Add next line in settings.py
+"""
+# Add next line in settings.py
 AUTH_USER_MODEL = ''
 """
