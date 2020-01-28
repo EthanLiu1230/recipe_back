7763d1dabd7f5f93ccc4c37a5bd624e1b5645dad
Ethan Liu <liuyuxuan1230@outlook.com>
2020-01-29 05:23:24 +0800

    Add Tag create api using mixin

--- recipe/views.py
+++ recipe/views.py
@@ -8,7 +8,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """
     Manage tags in the database.
     1. requires auth
@@ -23,3 +25,13 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Filter objects for the current authenticated user only."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new Tag.
+        mixed from CreateModelMixin,
+        :arg
+        serializer
+        is instance of serializer_class
+        """
+        # provide the fk information
+        serializer.save(user=self.request.user)
