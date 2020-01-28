e8408f067ca97c52d420abe6a523378b44ce9660
Ethan Liu <liuyuxuan1230@outlook.com>
2020-01-29 05:15:29 +0800

    Add Tag viewSet, Tag serializer , & router for the viewSet.

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+# combine view_sets and mixins to create list_Model_view_set
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """
+    Manage tags in the database.
+    1. requires auth
+    2. list tags
+    3. tags are specific for the user that is authenticated
+    """
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+    def get_queryset(self):
+        """Filter objects for the current authenticated user only."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
--- recipe_back/settings.py
+++ recipe_back/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # My apps
     'core',
     'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
